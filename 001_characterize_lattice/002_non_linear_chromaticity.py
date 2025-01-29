9b84d4f38dcf39ab0e8b855d785be9fa4fcb3e93
giadarol <giovanni.iadarola@cern.ch>
2025-01-29 07:20:13 +0100

    Example, high order chroma

---
+++ 001_characterize_lattice/002_non_linear_chromaticity.py
@@ -0,0 +1,36 @@
+import xtrack as xt
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.close('all')
+
+env = xt.Environment()
+env.call('../fccee_z_lattice.py')
+env.call('../fccee_z_strengths.py')
+
+line = env['fccee_p_ring']
+
+tw = line.twiss4d()
+
+nlc = line.get_non_linear_chromaticity(delta0_range=(-0.005, 0.005),num_delta=20,
+                                       fit_order=10)
+
+# High order chromaticity values
+print('\nHigh order chromaticity values:')
+print(f"Q'x =    {nlc.dnqx[1]:.2e}")
+print(f"Q''x =   {nlc.dnqx[2]:.2e}")
+print(f"Q'''x =  {nlc.dnqx[3]:.2e}")
+print(f"Q''''x = {nlc.dnqx[4]:.2e}")
+print(f"Q'y =    {nlc.dnqy[1]:.2e}")
+print(f"Q''y =   {nlc.dnqy[2]:.2e}")
+print(f"Q'''y =  {nlc.dnqy[3]:.2e}")
+print(f"Q''''y = {nlc.dnqy[4]:.2e}")
+
+plt.figure(1)
+plt.plot(nlc.delta0, nlc.qx - tw.qx, label=r'$\Delta Q_x$')
+plt.plot(nlc.delta0, nlc.qy - tw.qy, label=r'$\Delta Q_y$')
+plt.legend()
+plt.xlabel(r'$\delta_0$')
+plt.ylabel(r'$\Delta Q$')
+
+plt.show()
