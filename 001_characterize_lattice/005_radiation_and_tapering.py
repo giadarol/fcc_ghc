22799332a4a62d96aadb909ef0966e921d0f5a36
giadarol <giovanni.iadarola@cern.ch>
2025-02-03 22:03:01 +0100

    Better

--- 001_characterize_lattice/005_radiation_and_tapering.py
+++ 001_characterize_lattice/005_radiation_and_tapering.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import xtrack as xt
 
 # Z configuration
-env = xt.Environment.from_json('../fccee_t_thick_thin.json.gz')
+env = xt.Environment.from_json('../fccee_z_thick_thin.json.gz')
 env.call('../toolkit/install_rf_cavities_rpo.py') # Install realistic rf cavities
 env['l400_2'] = 0.5 # For twiss 6d we set cavity lags to 180 degrees, tapering will adjust the phase
 
@@ -25,5 +25,5 @@ line.compensate_radiation_energy_loss()
 tw = line.twiss(eneloss_and_damping=True)
 
 plt.close('all')
-tw.plot('delta')
+tw.plot('delta', lattice=False)
 plt.show()
