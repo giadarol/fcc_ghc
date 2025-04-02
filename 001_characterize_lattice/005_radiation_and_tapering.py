6d81223a845a47676bc786a93230c6cc4e9d6472
giadarol <giovanni.iadarola@cern.ch>
2025-04-02 22:02:13 +0200

    t doesnt like model/integrator

--- 001_characterize_lattice/005_radiation_and_tapering.py
+++ 001_characterize_lattice/005_radiation_and_tapering.py
@@ -2,18 +2,19 @@ import matplotlib.pyplot as plt
 
 import xtrack as xt
 
-# Z configuration
 env = xt.Environment()
-env.call('../fccee_z_lattice.py')
-env.call('../fccee_z_strengths.py')
 
-env.call('../toolkit/install_rf_cavities_rpo.py') # Install realistic rf cavities
-env['l400_2'] = 0.5 # For twiss 6d we set cavity lags to 180 degrees, tapering will adjust the phase
+# Z configuration
+# env.call('../fccee_z_lattice.py')
+# env.call('../fccee_z_strengths.py')
+# env.call('../toolkit/install_rf_cavities_rpo.py') # Install realistic rf cavities
+# env['l400_2'] = 0.5 # For twiss 6d we set cavity lags to 180 degrees, tapering will adjust the phase
 
-# # t configuration
-# env = xt.Environment.from_json('../fccee_t_thick_thin.json.gz')
-# env['lagca1'] = 0.5
-# env['lagca2'] = 0.5
+# t configuration
+env.call('../fccee_t_lattice.py')
+env.call('../fccee_t_strengths.py')
+env['lagca1'] = 0.5
+env['lagca2'] = 0.5
 
 # Get thin line
 line = env.fccee_p_ring
